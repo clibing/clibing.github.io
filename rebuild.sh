0a2524c29ca4c1489b1f1b86080f71ec7164a6d6
hp <hp@linuxcrypt.cn>
2018-01-20 17:07:43 +0800

    调整脚本顺序

--- rebuild.sh
+++ rebuild.sh
@@ -4,19 +4,20 @@
 BLOG_HOME="/home/blog/clibing.github.io"
 GIT_PULL_LOG="/tmp/git.pull.log"
 
+# git pull
+cd ${BLOG_HOME}
+git pull > ${GIT_PULL_LOG}
+GIT_STATUS=`cat /tmp/git.pull.log | awk '{print $1}'`
+if [ ${GIT_STATUS} = 'Already' ]; then
+    echo -e "`date '+%Y-%m-%d %H:%M:%S'` git none update"
+    exit 1
+fi
+
 # kill jekyll
 echo -e "`date '+%Y-%m-%d %H:%M:%S'` kill jekyll"
 pkill -f jekyll
 
-# git pull
-cd ${BLOG_HOME}
-git pull > ${GIT_PULL_LOG}
-
-GIT_STATUS=`cat /tmp/git.pull.log | awk '{print $1}'`
-
-if [ $GIT_STATUS = 'Already' ]; then
-    exit 1
-fi
+# start serve
 /usr/local/rvm/gems/ruby-2.3.0/bin/jekyll serve -H 0.0.0.0 -P 8080 --detach
 
 
