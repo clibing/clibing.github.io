612ef0b09b530f1117811376bbbe3c676d52672f
hp <hp@linuxcrypt.cn>
2018-01-20 17:02:54 +0800

    修改脚本增加条件判断

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,7 @@
 
 # env
 BLOG_HOME="/home/blog/clibing.github.io"
+GIT_PULL_LOG="/tmp/git.pull.log"
 
 # kill jekyll
 echo -e "`date '+%Y-%m-%d %H:%M:%S'` kill jekyll"
@@ -9,8 +10,13 @@ pkill -f jekyll
 
 # git pull
 cd ${BLOG_HOME}
-git pull
+git pull > ${GIT_PULL_LOG}
 
+GIT_STATUS=`cat /tmp/git.pull.log | awk '{print $1}'`
+
+if [ $GIT_STATUS = 'Already' ]; then
+    exit 1
+fi
 /usr/local/rvm/gems/ruby-2.3.0/bin/jekyll serve -H 0.0.0.0 -P 8080 --detach
 
 
