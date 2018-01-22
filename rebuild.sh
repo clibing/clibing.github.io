7bc7f01837c713b1710f3e15b9395c968c84a62e
root <root@clibing.github.io>
2018-01-22 22:02:35 +0800

    update restart shell

--- rebuild.sh
+++ rebuild.sh
@@ -18,5 +18,9 @@ echo -e "`date '+%Y-%m-%d %H:%M:%S'` kill jekyll">>${GIT_PULL_LOG}
 ps -aef|grep jekyll | grep 'ruby' | awk '{print $2}' | xargs kill -9 >>${GIT_PULL_LOG}
 
 # start serve
-rvm requirements && rvm install 2.3.0 && rvm use 2.3.0 --default && gem install bundler && bundle install >>${GIT_PULL_LOG}
+rvm requirements
+rvm install 2.3.0 
+rvm use 2.3.0 --default 
+gem install bundler 
+bundle install
 /usr/local/rvm/gems/ruby-2.3.0/bin/jekyll serve -H 0.0.0.0 -P 8080 --detach >>${GIT_PULL_LOG}
